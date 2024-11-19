244c18b4b4f2554f5fee9118c2cd5e93ed77c625
Felipe Schner <99690161+fschner@users.noreply.github.com>
2024-11-19 20:09:42 -0300

    Add files via upload

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -3,6 +3,12 @@
 sudo yum -y install epel-release
 echo "Inicio da instalação do Ansible"
 sudo yum -y install ansible
+sudo dnf install git -y
+git config --global user.name "fschner"
+git config --global user.email "fschner@gmail.com"
+cd /home/vagrant
+git clone https://github.com/fschner/ansible-lab
+ansible-galaxy install geerlingguy.mysql
 cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
